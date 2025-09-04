4c74a15c11a2d208495a0ef73a2b849bddc4fc22
alewaya <a.nicastro@wayassauto.com>
2025-09-04 21:44:09 +0200

    First commit

---
+++ app/ui/signup-form.tsx
@@ -0,0 +1,21 @@
+import { signup } from '@/app/actions/auth'
+ 
+export function SignupForm() {
+  return (
+    <form action={signup}>
+      <div>
+        <label htmlFor="name">Name</label>
+        <input id="name" name="name" placeholder="Name" />
+      </div>
+      <div>
+        <label htmlFor="email">Email</label>
+        <input id="email" name="email" type="email" placeholder="Email" />
+      </div>
+      <div>
+        <label htmlFor="password">Password</label>
+        <input id="password" name="password" type="password" />
+      </div>
+      <button type="submit">Sign Up</button>
+    </form>
+  )
+}
