188d97c503aa2716c573e6611ea7a0764bf07bff
alewaya <a.nicastro@wayassauto.com>
2026-02-02 10:58:03 +0100

    Modify the outline button variant to use gray color for hover background. Much better now, still need to understand and fix the dark mode of the component and to understand the bad hovering color of the button for hiding and showing the sidebar.

--- components/shadcn/ui/button.tsx
+++ components/shadcn/ui/button.tsx
@@ -13,7 +13,7 @@ const buttonVariants = cva(
         destructive:
           'bg-destructive text-destructive-foreground hover:bg-warning-hover focus-visible:ring-destructive/20 dark:focus-visible:ring-destructive/40',
         outline:
-          'border bg-background shadow-xs hover:bg-accent hover:text-accent-foreground dark:bg-input/30 dark:border-input dark:hover:bg-input/50',
+          'border bg-background shadow-xs hover:bg-sidebar-accent dark:bg-input/30 dark:border-input dark:hover:bg-input/50',
         secondary: 'bg-secondary text-secondary-foreground hover:bg-secondary-hover',
         ghost: 'hover:bg-accent hover:text-accent-foreground dark:hover:bg-accent/50',
         link: 'text-primary underline-offset-4 hover:underline',
